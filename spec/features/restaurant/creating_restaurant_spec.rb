e705a87d50c0c405e3f8cb360f4a993b20dafa6e
Keith Amoah <ka33145@googlemail.com>
2017-12-27 23:23:38 +0000

    Added creating_restaurant_spec.rb

---
+++ spec/features/restaurant/creating_restaurant_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+RSpec.feature 'Creating restaurant' do
+
+  scenario 'user create a new restaurant' do
+    visit '/'
+    click_link 'New Restaurant'
+    # fill_in 'Title', with: 'Creating a blog'
+    # fill_in 'Body', with: 'Lorem'
+    my_restaurant = {
+        name: 'Nando',
+        description: 'Chicken',
+        address1: '5 Lomdon Close',
+        address2: 'Sparclls',
+        city:  'Swindon',
+        county:  'Wiltshire',
+        postcode: 'SN6 5FF'
+    }
+    fill_form(my_restaurant)
+    click_button 'Save Restaurant'
+    expect(page).to have_content('Restaurant has been created')
+    expect(page.current_path).to eq(restaurants_path)
+  end
+  scenario 'User fails to create a new restaurant' do
+    visit '/'
+    click_link 'New Restaurant'
+     my_restaurant = {
+         name: '',
+         description: '',
+         address1: '',
+         address2: '',
+         city: '',
+         county: '',
+         postcode: ''
+     }
+    fill_form(my_restaurant)
+    click_button 'Save Restaurant'
+    expect(page).to have_content('Restaurant has not been created')
+    expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Description can't be blank")
+    expect(page).to have_content("Address1 can't be blank")
+    expect(page).to have_content("City can't be blank")
+    expect(page).to have_content("Postcode can't be blank")
+  end
+
+end
