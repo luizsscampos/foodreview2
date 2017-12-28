af83d6c06ea98aec2efc4987eedf653702e88581
Keith Amoah <ka33145@googlemail.com>
2017-12-28 22:05:15 +0000

    Added create method and whitelisted form fields from restaurants/new.html.erb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,19 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
   
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    flash[:sucess]= 'Restaurant has been created'
+    redirect_to restaurants_path
+    
+  end
+
+  private
+  
+  # Whitelist fields
+  def restaurant_params
+    params.require(:restaurant).permit(:name,:description,:address1,:address2,:city,:county,:postcode)
+  end
+    
 end
