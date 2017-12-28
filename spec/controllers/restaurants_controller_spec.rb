a0e83bc38f4c7ea8f7c4f7ae8619b1d2466071d0
Keith Amoah <ka33145@googlemail.com>
2017-12-28 02:33:51 +0000

    Correct filename/directory typo

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe RestaurantController, type: :controller do
+RSpec.describe RestaurantsController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
--- spec/helpers/restaurants_helper_spec.rb
+++ spec/helpers/restaurants_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe RestaurantHelper, type: :helper do
+RSpec.describe RestaurantsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
