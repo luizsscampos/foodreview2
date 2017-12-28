1dda7f0f99a506fde2957adfb1356a0af741113e
Keith Amoah <ka33145@googlemail.com>
2017-12-28 21:46:51 +0000

    Fixed migration and generate restaurant table

--- db/migrate/20171228035657_create_restaurants.rb
+++ db/migrate/20171228035657_create_restaurants.rb
@@ -1,8 +1,14 @@
 class CreateRestaurants < ActiveRecord::Migration[5.1]
   def change
-    create_table :restaurants do |t|
-
-      t.timestamps
+    create_table :restaurants do |restaurant|
+      restaurant.string :name
+      restaurant.text :description
+      restaurant.string :address1
+      restaurant.string :address2
+      restaurant.string :city
+      restaurant.string :county
+      restaurant.string :postcode
+      restaurant.timestamps
     end
   end
 end
