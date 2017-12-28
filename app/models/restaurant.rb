cadaf698a397498f12adba5b09ff576f4195f1e9
Keith Amoah <ka33145@googlemail.com>
2017-12-28 23:28:07 +0000

    Added restaurant model validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 3 }
+    validates :description, presence: true, length: { minimum: 3 }
+    validates :address1, presence: true, length: { minimum: 3 }
+    validates :city, presence: true, length: { minimum: 3 }
+    validates :postcode, presence: true, length: { minimum: 3, maximum: 10 }
 end
