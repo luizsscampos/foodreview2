33405a3e22b95ba32232c98ca2d7ba6dd65ad475
Keith Amoah <ka33145@googlemail.com>
2017-12-30 16:56:19 +0000

    Added spec for viewing restaurants

---
+++ spec/features/restaurant/view_restaurants_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+RSpec.feature 'View restaurants' do
+  before do
+    @restaurant1 = Restaurant.create(
+      name: 'Nando',
+      description: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).',
+      address1: '5 Lomdon Close',
+      address2: 'Sparclls',
+      city: 'Swindon',
+      county: 'Wiltshire',
+      postcode: 'SN6 5FF'
+    )
+    @restaurant2 = Restaurant.create(
+      name: 'IIL TOSCANO RISTORANTE',
+      description: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
+      address1: '6-7 station Parade Brighton Road',
+      address2: '',
+      city:  ' Sutton',
+      county:  'Surrey',
+      postcode: 'SM2 5AD'
+    )
+  end
+  scenario 'No restaurant avaliable' do
+    name = @restaurant2.name
+    desc = @restaurant1.description
+    Restaurant.delete_all
+    visit '/'
+    expect(page).to have_content 'No restaurants avaliable'
+    expect(page).not_to have_content(desc[0..96])
+    expect(page).not_to have_content name
+  end
+  scenario 'Restaurants avaliable' do
+    visit '/'
+    expect(page).not_to have_content 'No restaurants avaliable'
+    expect(page).to have_content @restaurant1.name
+      expect(page).to have_content("#{@restaurant2.description[0..96]}")
+  end
+end
