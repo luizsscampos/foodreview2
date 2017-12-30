1e2c3a7095ba084ad36235cc17f454fe08adaf70
Keith Amoah <ka33145@googlemail.com>
2017-12-30 16:47:12 +0000

    Prepopulate restaurants table with two restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@restaurant9 = Restaurant.where(name: 'Amigos Mexican Restaurant').first_or_create(
+  name: 'Amigos Mexican Restaurant',
+  description: Faker::Lorem.paragraph(5,true,7),
+  address1: '41 Friar Street',
+  address2: '',
+  city:  'Worchester',
+  county:  '',
+  postcode: 'WR1 2NA'#,
+#   image: File.open("#{path}amigos-min.png")
+)
+
+@restaurant10 = Restaurant.where(name: 'Lebaneat').first_or_create(
+  name: 'Lebaneat',
+  description: Faker::Lorem.paragraph(4,true,8),
+  address1: '47 North Baliey',
+  address2: '',
+  city:  'Durham',
+  county:  '',
+  postcode: 'DH1 3ET'#,
+#   image: File.open("#{path}lebaneat-min.png")
+)
