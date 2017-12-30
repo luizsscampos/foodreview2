81bfad9ddf2c1f02c52bc5bb19fd54d5f4703513
Keith Amoah <ka33145@googlemail.com>
2017-12-30 21:07:45 +0000

    Added more restuarant sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@restaurant5 =Restaurant.where(name: 'Infamous Diner').first_or_create(
+  name: 'Infamous Diner',
+  description: Faker::Lorem.paragraph(8,true,2),
+  address1: '3-5 Basil Chambers Nicholas Croft',
+  address2: '',
+  city:  'Manchester',
+  county:  '',
+  postcode: 'M4 1EY'#,
+#   image: File.open("#{path}infamous_diner-min.png")
+)
+
+@restaurant6 = Restaurant.where(name: 'Los Gatos').first_or_create(
+  name: 'Los Gatos',
+  description: Faker::Lorem.paragraph(7,true,3),
+  address1: '1-3 Devizes Road',
+  address2: 'Old Town',
+  city:  'Swindon',
+  county:  '',
+  postcode: 'SN4 4BJ'#,
+#   image: File.open("#{path}los_gatos-min.png")
+)
+
+@restaurant7 = Restaurant.where(name: 'Nur').first_or_create(
+  name: 'Nur',
+  description: Faker::Lorem.paragraph(6,true,4),
+  address1: '1-3 Devizes Road',
+  address2: '',
+  city:  'Glasgow',
+  county:  '',
+  postcode: 'G5 9HR'#,
+#   image: File.open("#{path}nur-min.png")
+)
+
+@restaurant8 = Restaurant.where(name: 'Thai Garden').first_or_create(
+  name: 'Thai Garden',
+  description: Faker::Lorem.paragraph(2,true,9),
+  address1: '100 West Street',
+  address2: '',
+  city:  'Bristol',
+  county:  '',
+  postcode: 'BS3 3LR'#,
+#   image: File.open("#{path}thai_garden-min.png")
+)
+
 @restaurant9 = Restaurant.where(name: 'Amigos Mexican Restaurant').first_or_create(
   name: 'Amigos Mexican Restaurant',
   description: Faker::Lorem.paragraph(5,true,7),
