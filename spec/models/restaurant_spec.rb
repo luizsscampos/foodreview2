99fcd5ccd4fa575ad2f21b57a1c2776caa2f742f
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-10 17:37:51 +0000

    Refactored restaurant model spec

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,40 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  subject {described_class.new(
-    name: 'Amigos Mexican Restaurant',
-    description: Faker::Lorem.paragraph(5, true, 7),
-    address1: '41 Friar Street',
-    address2: '',
-    city:  'Worchester',
-    county:  '',
-    postcode: 'WR1 2NA'
-    )}
+  subject(:restaurant1) do
+    described_class.new(
+      name: 'Amigos Mexican Restaurant',
+      description: Faker::Lorem.paragraph(5, true, 7),
+      address1: '41 Friar Street',
+      address2: '',
+      city:  'Worchester',
+      county:  '',
+      postcode: 'WR1 2NA'
+    )
+  end
   it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+    expect(restaurant1).to be_valid
   end
   it 'is not valid with a name shorter 3 characters' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+    restaurant1.name = nil
+    expect(restaurant1).to_not be_valid
   end
   it 'is not valid with a description shorter 3 characters' do
-    subject.description = 'am'
-    expect(subject).to_not be_valid
+    restaurant1.description = 'am'
+    expect(restaurant1).to_not be_valid
   end
   it 'is not valid with a address1 shorter 3 characters' do
-    subject.address1 = ''
-    expect(subject).to_not be_valid
+    restaurant1.address1 = ''
+    expect(restaurant1).to_not be_valid
   end
   it 'is not valid with a city shorter 3 characters' do
-    subject.city = 'N'
-    expect(subject).to_not be_valid
+    restaurant1.city = 'N'
+    expect(restaurant1).to_not be_valid
   end
-  it 'is not valid with a postcode shorter 3 characters' do
-    subject.postcode = 'am'
-    expect(subject).to_not be_valid
-  end
-  it 'is not valid with a postcode longer 10 characters' do
-    subject.postcode = 'SN10 O5DGO0'
-    expect(subject).to_not be_valid
+  it 'is not valid with a inavlid postcode' do
+    restaurant1.postcode = 'am'
+    expect(restaurant1).to_not be_valid
+    restaurant1.postcode = 'SN10 O5DGO0'
+    expect(restaurant1).to_not be_valid
   end
 end
