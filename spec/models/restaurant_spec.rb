99e074d11018c25d51e2b9699950f1d1474e1c7c
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-10 10:48:26 +0000

    Added validation tests on Restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {described_class.new(
+    name: 'Amigos Mexican Restaurant',
+    description: Faker::Lorem.paragraph(5, true, 7),
+    address1: '41 Friar Street',
+    address2: '',
+    city:  'Worchester',
+    county:  '',
+    postcode: 'WR1 2NA'
+    )}
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+  it 'is not valid with a name shorter 3 characters' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid with a description shorter 3 characters' do
+    subject.description = 'am'
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid with a address1 shorter 3 characters' do
+    subject.address1 = ''
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid with a city shorter 3 characters' do
+    subject.city = 'N'
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid with a postcode shorter 3 characters' do
+    subject.postcode = 'am'
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid with a postcode longer 10 characters' do
+    subject.postcode = 'SN10 O5DGO0'
+    expect(subject).to_not be_valid
+  end
 end
