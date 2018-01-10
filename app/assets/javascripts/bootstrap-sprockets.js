e619e4b7cd23317ae854af375d8d17ebd64c2895
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-10 11:39:14 +0000

    Added custom bootstrap-sprockets.js

---
+++ app/assets/javascripts/bootstrap-sprockets.js
@@ -0,0 +1,14 @@
+//= require bootstrap/transition
+//= require bootstrap/alert
+//= require bootstrap/button
+
+//= require bootstrap/collapse
+//= require bootstrap/dropdown
+//= require bootstrap/modal
+
+//= require bootstrap/affix
+//= require bootstrap/scrollspy
+//= require bootstrap/tooltip
+
+// See Bootstrap-sass for default bootstrap-sprockets
+// https://github.com/twbs/bootstrap-sass/blob/master/assets/javascripts/bootstrap-sprockets.js
