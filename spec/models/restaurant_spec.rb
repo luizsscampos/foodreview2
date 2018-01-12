bf4e7c1894244bc9db24e78dac17cff23d391688
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-12 23:18:52 +0000

    Added test for Restaurant model full_address method

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -37,4 +37,28 @@ RSpec.describe Restaurant, type: :model do
     restaurant1.postcode = 'SN10 O5DGO0'
     expect(restaurant1).to_not be_valid
   end
+  describe '#full_address' do
+    let(:address1) { restaurant1.address1 }
+    let(:address2) { restaurant1.address2 }
+    let(:city) { restaurant1.city }
+    let(:county) { restaurant1.county}
+    let(:postcode) { restaurant1.postcode }
+    context 'with partial address' do
+      it 'return format address' do
+        myaddress = [address1, city, postcode].join(', ')
+        expect(restaurant1.full_address).to eq myaddress
+      end
+    end
+    context 'with full address' do
+      before do
+        restaurant1.address2 = 'address2'
+        restaurant1.county = 'county'
+      end
+      it 'return format address' do
+        myaddress = [address1, address2, city, county, postcode].join(', ')
+        puts myaddress
+        expect(restaurant1.full_address).to eq myaddress
+      end
+    end
+  end
 end
