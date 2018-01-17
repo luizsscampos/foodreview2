d8f5f9334efba4960f1eb89d5a768232b2cdded8
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-17 00:52:40 +0000

    Added category to show.html.erb and updated show restaurant specs

--- spec/features/restaurants/show_restaurant_spec.rb
+++ spec/features/restaurants/show_restaurant_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 require_relative '../../support/shared_stuff'
 RSpec.feature 'Show restaurants' do
   include_context 'shared_stuff'
-  scenario 'User view a restaurant' do
-    visit '/'
+  include Helper
+  scenario 'User click on restaurant name' do
+    visit_homepage
     within('//li#restaurant2') do
       click_link(@restaurant2.name)
     end
@@ -11,5 +12,14 @@ RSpec.feature 'Show restaurants' do
     expect(page).to have_content @restaurant2.name
     expect(page).to have_content @restaurant2.description
     expect(page).to have_content @restaurant2.full_address
+    categories_titles = @restaurant2.categories.map(&:title).join(' ')
+    expect(page).to have_content categories_titles
+  end
+  scenario 'User click continue on a restaurant' do
+    visit_homepage
+    within('//li#restaurant1') do
+      click_link('Continue')
+    end
+    expect(page).to have_current_path restaurant_path(@restaurant1)
   end
 end
