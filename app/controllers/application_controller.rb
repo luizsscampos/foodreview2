eda4dae7d96d51476a26a8d3108acb5f0a8b330e
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-20 00:42:30 +0000

    Added method to whitelist user fields for sign up, login and update account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,18 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def resource_not_found; end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name,:last_name,:username,:email,:tel ,:password, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:first_name,:last_name,:username,:email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:first_name,:last_name,:username,:tel ,:email, :password, :remember_me])
+  end
 end
+# def configure_permitted_parameters
+#     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, address_attributes: [:country, :state, :city, :area, :postal_code]])
+#   end
