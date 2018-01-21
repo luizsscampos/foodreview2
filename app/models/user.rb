430355081dd4c74fccf3ab75bf30fe58bbe55e7a
keith(MacBookPro) <ka33145@googlemail.com>
2018-01-21 20:36:46 +0000

    Added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, length: { minimum: 3 }
+  validates :last_name, length: { minimum: 3 }
+  validates :username, length: { minimum: 3 }, uniqueness: true
+  validates :tel, length: { in: 11..12 }
+
   def full_name
     [first_name, last_name].join(' ')
   end
